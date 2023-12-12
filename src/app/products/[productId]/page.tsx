d674f73fa94fd53f868ef47382db95b6d02e52df
ART1377 <alirezatt705@gmail.com>
2023-12-12 20:28:34 +0330

    main layout metadata changed and generateMetadata added to product list page

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,11 +1,29 @@
 import React from "react";
 import type { Metadata } from "next";
+import { products } from "@/app/db/db";
+import { Product } from "../../../../next-type-d";
 
 type Props = {
   params: {
     productId: string;
   };
 };
+
+export function generateMetadata({
+  params: { productId },
+}: Props): // Promise<Metadata>
+Metadata {
+  const id = productId;
+
+  const productData = products.find(
+    (product: Product) => product.productId === id
+  );
+
+  return {
+    title: productData?.productName,
+    description: productData?.productDescription,
+  };
+}
 
 const productDetailPage = ({ params: { productId } }: Props) => {
   return (
