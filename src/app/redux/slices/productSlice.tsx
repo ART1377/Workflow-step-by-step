d9deb90749eacaf949cf9a5bcd296204e2bea22f
ART1377 <alirezatt705@gmail.com>
2023-12-14 15:41:57 +0330

    redux added to project (productSlice created and hooks aswell)

---
+++ src/app/redux/slices/productSlice.tsx
@@ -0,0 +1,104 @@
+import { createSlice, createAsyncThunk, PayloadAction } from '@reduxjs/toolkit';
+import axios from 'axios';
+// import { RootState } from '../store'; 
+import { Product, Step } from "../../../../next-type-d";
+
+const API_URL = "http://127.0.0.1:3500/products";
+
+export interface InitialStateType {
+  products: Product[];
+  status: string;
+  error: null | string;
+}
+const initialState: InitialStateType = {
+  products: [],
+  status: "idle", //'idle' | 'loading' | 'succeeded' | 'failed'
+  error: null,
+};
+
+
+export const fetchProducts = createAsyncThunk('products/fetchProducts', async () => {
+  const response = await axios.get(`${API_URL}/products`);
+  return response.data;
+});
+
+
+export const updateProduct = createAsyncThunk(
+  'products/updateProduct',
+  async ({
+    productId,
+    updatedStep,
+  }: {
+    productId: string;
+    updatedStep: { person: { personName: string; personId: string }; state: string; step: number };
+  }) => {
+    const response = await axios.get(`${API_URL}/products/${productId}`);
+    const updatedProduct = response.data;
+
+    // Find the index of the step to update
+    const stepIndex = updatedProduct.steps.findIndex(
+      (step:Step) => step.person.personId === updatedStep.person.personId
+    );
+
+    // If the step is found, update it
+    if (stepIndex !== -1) {
+      updatedProduct.steps[stepIndex] = updatedStep;
+    }
+
+    // Now, update the product on the server
+    await axios.put(`${API_URL}/products/${productId}`, updatedProduct);
+
+    // Return the updated step
+    return updatedStep;
+  }
+);
+
+
+const productSlice = createSlice({
+  name: "product",
+  initialState,
+  reducers: {},
+  extraReducers(builder) {
+    builder
+      .addCase(fetchProducts.pending, (state, action) => {
+        state.status = "loading";
+      })
+      .addCase(fetchProducts.fulfilled, (state, action) => {
+        state.status = "succeeded";
+        state.products = action.payload;
+      })
+      .addCase(fetchProducts.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = "something went wrong";
+      })
+
+      .addCase(updateProduct.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(updateProduct.fulfilled, (state, action) => {
+        state.status = 'succeeded';
+        // No need to update the entire state, only update the specific product's step
+        const productId = action.payload.person.personId;
+        const updatedStep = action.payload;
+
+        state.products = state.products.map((product:Product) =>
+          product.productId === productId
+            ? {
+                ...product,
+                steps: product.steps.map((step:Step) =>
+                  step.person.personId === updatedStep.person.personId ? updatedStep : step
+                ),
+              }
+            : product
+        );
+      })
+      .addCase(updateProduct.rejected, (state, action) => {
+        state.status = 'failed';
+        state.error = action.error.message || 'Failed to update product';
+      });
+
+     
+  },
+});
+
+export default productSlice.reducer;
