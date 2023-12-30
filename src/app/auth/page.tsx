c03c0793bad67fb4086c3fc30ed4aa052d00182c
ART1377 <alirezatt705@gmail.com>
2023-12-30 20:13:06 +0330

    login page changed

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -13,16 +13,16 @@ const page = (props: Props) => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
-  const handleLogin = () => {
+  const loginHandler = () => {
     authCtx?.login({ username, password });
   };
 
   return (
-    <div className="bg-light flex justify-between items-center gap-6 p-4 rounded-radius-large min-h-[350px]">
+    <div className="bg-light flex justify-between items-center gap-6 p-4 rounded-radius-large mr-3 min-h-[350px]">
       <div className="w-[40%] h-full">
         <div className="flex flex-col gap-6">
           <h5 className="font-bold text-primary-main text-center">Login</h5>
-          <form className="flex flex-col gap-6">
+          <form onSubmit={loginHandler} className="flex flex-col gap-6">
             <Input
               label="Username"
               type="text"
@@ -36,14 +36,14 @@ const page = (props: Props) => {
               onChange={(e) => setPassword(e.target.value)}
             />
 
-            <Button type="button" variant="primary-main" onClick={handleLogin}>
+            <Button type="submit" variant="primary-main" >
               Login
             </Button>
           </form>
         </div>
       </div>
-      <div className="w-[60%]">
-        <div className="relative w-full h-full rounded-radius-main overflow-hidden">
+      <div className="w-[60%] h-full">
+        <div className="relative w-full h-full min-h-[350px] rounded-radius-main overflow-hidden">
           <Image src={shoppingImage} alt="login image" fill />
         </div>
       </div>
